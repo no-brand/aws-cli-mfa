c934e90711cfc4ba724ae4fa5b3f2f05a4ebd818
no-brand <do.dream.david@gmail.com>
2021-01-09 22:27:44 +0900

    chore: project created

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re
+
+with open('aws-cli-mfa/__init__.py', encoding='utf8') as f:
+    version = re.search(r'__version__ = \'(.*)\'', f.read()).group(1)
+    
+setup(
+    name='aws-cli-mfa',
+    version=version,
+    description='',
+    install_requires=['boto3'],
+    packages=find_packages(exclude=['tests*']),
+    author='no-brand',
+    author_email='do.dream.david@gmail.com',
+    url='https://github.com/no-brand/aws-cli-mfa',
+    license='Apache-2.0 License'
+)

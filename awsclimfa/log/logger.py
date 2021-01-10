34b02afde7bbd625ce7c0d5b71e33e85af60d985
no-brand <do.dream.david@gmail.com>
2021-01-10 21:35:40 +0900

    feat: get session token implementation
    ~/.aws/credentials is used
    aws sts get-session-token 's response is write on it
    aws_profile naming rule with '-default'

--- awsclimfa/log/logger.py
+++ awsclimfa/log/logger.py
@@ -7,11 +7,12 @@ from logging import config
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self, level=logging.DEBUG):
         with open('{}/config.yaml'.format(os.path.dirname(__file__)), 'r') as f:
             dic = yaml.safe_load(f.read())
             config.dictConfig(dic)
         self.__logger = logging.getLogger('aws-cli-mfa')
+        self.__logger.setLevel(level)
 
     @property
     def logger(self):

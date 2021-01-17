d750bbc6f9b77c024a44e52ea96f975d93bb31fb
no-brand <do.dream.david@gmail.com>
2021-01-17 22:42:47 +0900

    chore: long description for pipy homepage

--- awsclimfa/__init__.py
+++ awsclimfa/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.0-beta'
+__version__ = '0.1.1-beta'
--- setup.py
+++ setup.py
@@ -7,10 +7,15 @@ import os
 with open('awsclimfa/__init__.py', encoding='utf8') as f:
     version = re.search(r'__version__ = \'(.*)\'', f.read()).group(1)
     
+with open('README.md', 'r') as f:
+    long_description = f.read() 
+
 setup(
     name='aws-cli-mfa',
     version=version,
-    description='',
+    description='It can help you to access AWS resources through AWS CLI with MFA token',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['boto3'],
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,

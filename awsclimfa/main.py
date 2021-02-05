e8b82a91215396a70414931f4a145015ab471ebd
no-brand <do.dream.david@gmail.com>
2021-02-05 22:13:10 +0900

    feat: assume role implementation
    ~/.aws/credentials is used
    aws sts assume-role's response is write on it
    
    issue: #3

--- awsclimfa/main.py
+++ awsclimfa/main.py
@@ -13,10 +13,12 @@ def run():
     AWS_CONFIG_CREDENTIALS_PATH = '{}/.aws/credentials'.format(os.path.expanduser('~'))
 
     # aws sts get-session-token --serial-number $MFA_DEVICE_ARN --token-code $TOKEN_CODE
-    parser = argparse.ArgumentParser(description='Get and enable your AWS session token')
-    parser.add_argument('--mfa', help='MFA token code (--token-code)', type=int, required=True)
-    parser.add_argument('--profile', help='AWS profile you want to use (--profile)', type=str, default=os.getenv('AWS_PROFILE'))
-    parser.add_argument('--arn', help='ARN of the MFA device (--serial-number)', type=str)
+    parser = argparse.ArgumentParser(description='Get and enable your AWS session token. Furthermore, ')
+    parser.add_argument('--mfa', help='MFA token code (get-session-token --token-code)', type=int, required=True)
+    parser.add_argument('--profile', help='AWS profile you want to use (get-session-token --profile)', type=str, default=os.getenv('AWS_PROFILE'))
+    parser.add_argument('--arn', help='ARN of the MFA device (get-session-token --serial-number)', type=str)
+    parser.add_argument('--assume_role', help='ARN of the role to assume (assume-role --role-arn)', type=str)
+    parser.add_argument('--assume_profile', help='AWS profile you want to get role to assume (assume-role)', type=str)
     args = parser.parse_args()
 
     if args.profile is None:
@@ -45,7 +47,7 @@ def run():
                             '--token-code', str(args.mfa)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if result.returncode != 0:
-        logger.debug('get-session-token if failed')
+        logger.debug('get-session-token is failed')
         parser.error(result.stderr.decode('utf-8').strip('\n'))
     else:
         logger.debug('get-session-token is succeeded')
@@ -58,6 +60,54 @@ def run():
         config[args.profile]['aws_secret_access_key'] = credentials['SecretAccessKey']
         config[args.profile]['aws_session_token'] = credentials['SessionToken']
         config[args.profile]['aws_session_expiration'] = credentials['Expiration']
+
+        with open(AWS_CONFIG_CREDENTIALS_PATH, 'w') as f:
+            config.write(f)
+
+    # Assuming role to be other account
+    if args.assume_role is not None:
+        if args.assume_profile is None:
+            parser.error('missing AWS profile, having role to assume')
+
+        # Remeber assume role arn
+        if not config.has_section(args.assume_profile):
+            config.add_section(args.assume_profile)
+        config[args.assume_profile]['assume_role_arn'] = args.assume_role
+        with open(AWS_CONFIG_CREDENTIALS_PATH, 'w') as f:
+            config.write(f)
+        logger.debug('write assume_role_arn ({}) into ~/.aws/credentials'.format(args.assume_role))
+    else:
+        args.assume_role = config[args.assume_profile]['assume_role_arn']
+        logger.debug('read mfa_device_arn ({}) from ~/.aws/credentials'.format(args.assume_role))
+
+    # Generate identifier of assuming role    
+    result = subprocess.run(['aws', 'iam', 'get-user', '--profile', args.profile,],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        logger.debug('get-user is failed')
+        parser.error(result.stderr.decode('utf-8').strip('\n'))
+    else:
+        logger.debug('get-user is succeeded')
+        user = json.loads(result.stdout.decode('utf-8'))['User']['UserName']
+    
+    # Get session token for assume-role
+    result = subprocess.run(['aws', 'sts', 'assume-role',
+                            '--role-arn', args.assume_role,
+                            '--role-session-name', user], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        logger.debug('assume-role is failed')
+        parser.error(result.stderr.decode('utf-8').strip('\n'))
+    else:
+        logger.debug('assume-role is succeeded')
+        credentials = json.loads(result.stdout.decode('utf-8'))['Credentials']
+        
+        config = configparser.ConfigParser()
+        config.read(AWS_CONFIG_CREDENTIALS_PATH)
+
+        config[args.assume_profile]['aws_access_key_id'] = credentials['AccessKeyId']
+        config[args.assume_profile]['aws_secret_access_key'] = credentials['SecretAccessKey']
+        config[args.assume_profile]['aws_session_token'] = credentials['SessionToken']
+        config[args.assume_profile]['aws_session_expiration'] = credentials['Expiration']
 
         with open(AWS_CONFIG_CREDENTIALS_PATH, 'w') as f:
             config.write(f)
